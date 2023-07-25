ad6e699cb2caf1687dd103760b78ecff835905d9
Martin <laumets.m@gmail.com>
2023-07-25 16:58:47 +0300

    Update CartContext.js

--- client/src/contexts/CartContext.js
+++ client/src/contexts/CartContext.js
@@ -4,47 +4,50 @@ import { GET_CART } from "../graphql/queries";
 
 export const CartContext = createContext([]);
 
-export const CartContextProvider = ({children})=>{
-    const {data} = useQuery(GET_CART,{variables:{
-        "cartid": parseInt(localStorage.getItem('cartId'))
-    }})
-    const [carts, setCart] = useState([]);
-
-    useEffect(()=>{
-        if(data?.getCart){
-            setCart(data.getCart)
-        }
-    }, [data])
-
-    const updateCart = (val)=>{
-        let isExist = false;
-        let newCart = carts.map(cart=>{
-            if(cart.productSku === val.productSku){
-                isExist = true;
-                return val;
-            }else {
-                return cart;
-            }
-        })
-
-        if(isExist){
-            setCart(newCart);
-        }else{
-            setCart([...carts, val]);
-        }
+export const CartContextProvider = ({ children }) => {
+  const { data } = useQuery(GET_CART, {
+    variables: {
+      "cartid": parseInt(localStorage.getItem('cartId'))
     }
+  });
+  const [carts, setCart] = useState([]);
 
-    const deleteCartItem = ((cartId, productSku)=>{
-        const updatedCartItems = carts.filter((cart) => !(cart.cartid === cartId && cart.productSku === productSku));
-        setCart(updatedCartItems);
-    })
-
-    const value = {
-        carts,
-        setCart,
-        updateCart,
-        deleteCartItem
+  useEffect(() => {
+    if (data?.getCart) {
+      setCart(data.getCart);
     }
+  }, [data]);
 
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
-}
+  const updateCart = (val) => {
+    let isExist = false;
+    let newCart = carts.map((cart) => {
+      if (cart.productSku === val.productSku) {
+        isExist = true;
+        return val;
+      } else {
+        return cart;
+      }
+    });
+
+    if (isExist) {
+      setCart(newCart);
+    } else {
+      setCart((prevCart) => [...prevCart, val]);
+    }
+  };
+
+  const deleteCartItem = (cartId, productSku) => {
+    setCart((prevCart) =>
+      prevCart.filter((cart) => !(cart.cartid === cartId && cart.productSku === productSku))
+    );
+  };
+
+  const value = {
+    carts,
+    setCart,
+    updateCart,
+    deleteCartItem
+  };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
